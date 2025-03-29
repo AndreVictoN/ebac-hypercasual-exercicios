f8f719e8267307ab3941e9dffb08be91c2a2dc3e
André Victor Gonçalves Nascimento <135927008+AndreVictoN@users.noreply.github.com>
2025-03-29 19:27:41 -0300

    Update and rename ItemCollectableBase.cs to ItemCollectibleBase.cs

--- Assets/Scripts/Items/ItemCollectibleBase.cs
+++ Assets/Scripts/Items/ItemCollectibleBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemCollectableBase : MonoBehaviour
+public class ItemCollectibleBase : MonoBehaviour
 {
     public string compareTag = "Player";
 
